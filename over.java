718d7b9ee7be0fd99f988c8a9f08ea07ef7fac74
Arun Tewatia <dadhotaarun@gmail.com>
2025-11-13 03:59:30 +0530

    Add files via upload

---
+++ over.java
@@ -0,0 +1,10 @@
+class over{
+    public static void main(String[] args) {
+        System.out.println("1");
+        over o = new over();
+        o.main(20);  
+    }
+    public static void main (int a ){
+        System.out.println("2");  
+    }
+} 

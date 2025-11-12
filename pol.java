cf76fbd7695905db57986ac4c455003a9c77bafd
Arun Tewatia <dadhotaarun@gmail.com>
2025-11-12 21:01:21 +0530

    Add files via upload

---
+++ pol.java
@@ -0,0 +1,13 @@
+class pol{
+    void abc(int a ){
+        System.out.println("I am here bro for your help ");
+    }
+    void abc(){
+        System.out.println("hy ");
+    }
+    public static void main(String[] args) {
+        pol p = new pol();
+        p.abc();    
+    }
+  
+}

c3b7addf65f8ab91375342e5de30f02a0cbb418d
Arun Tewatia <dadhotaarun@gmail.com>
2025-11-16 13:15:07 +0530

    Add files via upload

---
+++ abx.java
@@ -0,0 +1,13 @@
+public class abx {
+    void display(){
+        System.out.println("hello bro i am here for you ok ");
+    }
+    void show(){
+         display(); // compiler by default kiud se this keyword laga leta h // 
+    }                 // this is also this method // 
+    public static void main(String[] args) {
+        abx a = new abx();
+        a.show(); 
+    }
+    
+}

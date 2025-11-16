786c0a84187ace6852d7e0969da0034cd05d2994
Arun Tewatia <dadhotaarun@gmail.com>
2025-11-16 21:12:40 +0530

    Add files via upload

---
+++ st.java
@@ -0,0 +1,8 @@
+public class st {
+    static int a = 10;
+}
+class demo{
+    public static void main(String[] args) {
+        System.out.println(st.a);  // this is the static method in java bro //       
+    }
+}
